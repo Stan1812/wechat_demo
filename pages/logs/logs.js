32ec342695bb39ab5ba336729eae3ef07a513272
stan1812 <21314980309@qq.com>
2018-01-06 18:14:17 +0800

    add random article

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -4,13 +4,43 @@ var WxParse = require('../../wxParse/wxParse.js');
 Page({
   data: {
     artContent:{},
-    article:""
+    article:"",
+    date:{},
+    loadEnd:false
+  },
+  getRandom:function(){
+    let self = this
+    wx.pageScrollTo({
+      scrollTop: 0
+    })
+    wx.showLoading({
+      title: '加载中',
+    })
+    wx.request({
+      url: 'https://interface.meiriyiwen.com/article/random?dev=1',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        wx.hideLoading()
+        console.log(res.data)
+        self.setData({
+          artContent: res.data,
+          date: res.data,
+          loadEnd: true,
+          article: WxParse.wxParse('article', 'html', res.data.data.content, self, 5)
+        })
+      }
+    })
   },
   onLoad: function () {
     let self = this
     wx.showLoading({ 
       title: '加载中',
     })
+    // wx.onCompassChange(function (res) {
+    //   console.log(res.direction)
+    // })
     wx.request({
       url: 'https://interface.meiriyiwen.com/article/today?dev=1',
       header: {
@@ -18,8 +48,11 @@ Page({
       },
       success: function (res) {
         wx.hideLoading()
+        console.log(res.data)
         self.setData({
           artContent: res.data,
+          date:res.data,
+          loadEnd:true,
           article:WxParse.wxParse('article', 'html', res.data.data.content, self, 5)
         })
       }
